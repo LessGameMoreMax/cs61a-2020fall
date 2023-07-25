cd53e7b8a2ad0971d0d09cbc333268e61aae77b5
sablin <LessGameMoreMax@outlook.com>
2023-07-25 16:50:01 +0800

    23/07/25

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,17 +2,19 @@
 
 
 CREATE TABLE average_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT category, AVG(MSRP) AS average_price FROM products GROUP BY category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, MIN(price) AS price FROM inventory GROUP BY item;
 
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.item, a.store FROM
+    lowest_prices AS a, products AS b  WHERE b.name = a.item GROUP BY b.category HAVING MIN(b.MSRP/b.rating);
 
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT SUM(a.MBs) FROM
+    stores AS a, shopping_list AS b WHERE b.store = a.store;    
 
